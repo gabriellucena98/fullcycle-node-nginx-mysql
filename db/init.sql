549db6af0c4bae4c05e2b9c1932191d700a33370
gabriel.lucena <gabriel.lucena@ifood.com.br>
2025-05-16 09:48:36 -0300

    fix bugs

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS nodedb;
+
+USE nodedb;
+
+CREATE TABLE IF NOT EXISTS people (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+FLUSH PRIVILEGES;
